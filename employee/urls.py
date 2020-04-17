310eb8c0784e04eb51b35d57d6936218c14bc1fa
bind-ritu1211 <kumariritubind@gmail.com>
2020-04-18 05:13:31 +0530

    login 75% done

--- employee/urls.py
+++ employee/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
-from .views import HomeView
+from .views import HomeView,CheckInView
 
 urlpatterns = [
     path('index/', HomeView.as_view()),
-    
+    path('checkin/', CheckInView.as_view()),
+   
 ]
--- employee/views.py
+++ employee/views.py
@@ -6,3 +6,7 @@ from django .views .generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "index.html"
+
+
+class CheckInView(TemplateView):
+    template_name = "checkin.html"
