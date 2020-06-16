d3765b80ec0a6b3f6f201207dc24ffc5608f7c1b
bind-ritu1211 <kumariritubind.com>
2020-06-16 19:51:01 +0530

    data load

--- employee/views.py
+++ employee/views.py
@@ -1,15 +1,15 @@
+import _datetime
 from django.utils.decorators import method_decorator
 from django.shortcuts import render
 from django .views .generic import TemplateView
 from rest_framework.views import APIView
-from .models import EmployeeProfile,Attendance
+from .models import EmployeeProfile, Attendance
 from rest_framework.response import Response
-from .serializer import EmployeeProfileSerializer,AttendanceSerializer
+from .serializer import EmployeeProfileSerializer, AttendanceSerializer
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
 import datetime
 datetime.date.today()
-import _datetime
 now = datetime.datetime.now()
 today = _datetime.date.today()
 print(today)
@@ -25,7 +25,6 @@ class HomeView(TemplateView):
         return render(self.request, self.template_name, {})
 
 
-
 class AttendanceTemplateView(TemplateView):
     template_name = "common/attendance.html"
 
@@ -36,11 +35,10 @@ class AttendanceTemplateView(TemplateView):
 
 class AttendanceView(APIView):
     def get(self, request, format=None):
-        attendance = Attendance.objects.filter(employee_profile=self.request.user)
+        attendance = Attendance.objects.filter(
+            employee_profile=self.request.user)
         attendance = AttendanceSerializer(attendance, many=True)
-        return Response(attendance).data
-
-
+        return Response(attendance.data)
 
 
 class CheckOutTemplateView(TemplateView):
@@ -51,38 +49,36 @@ class CheckOutTemplateView(TemplateView):
         return render(self.request, self.template_name, {})
 
 
-
 class CheckInView(APIView):
     def post(self, request, *args):
-        is_availble_employee = Attendance.objects.filter(employee_profile=self.request.user)
+        is_availble_employee = Attendance.objects.filter(
+            employee_profile=self.request.user)
         if not is_availble_employee:
             if current_time == "10:00" or current_time < "10:30":
                 attendance = Attendance.objects.create(status="Full_Day",
-                                                employee_profile=self.request.user,
-                                                check_in= now,
-)
+                                                       employee_profile=self.request.user,
+                                                       check_in=now,
+                                                       )
             else:
-            #pass
-                attendance= Attendance.objects.create(status="Half_Day",
-                                                employee_profile=self.request.user,
-                                                check_in= now,
-)
+                # pass
+                attendance = Attendance.objects.create(status="Half_Day",
+                                                       employee_profile=self.request.user,
+                                                       check_in=now,
+                                                       )
             return Response("successfully checked in")
         else:
             return Response("already checked in")
 
 
-
-
 class CheckOutView(APIView):
     def post(self, request, *args):
-        attendance = Attendance.objects.filter(employee_profile=self.request.user)
+        attendance = Attendance.objects.filter(
+            employee_profile=self.request.user)
         if attendance:
-            #pass
-            attendance.update(check_out= now )
-        return Response("successfully check out") 
+            # pass
+            attendance.update(check_out=now)
+        return Response("successfully check out")
         #print("successfully check out")
-
 
 
 class EmployeeView(TemplateView):
@@ -91,12 +87,12 @@ class EmployeeView(TemplateView):
     @method_decorator(login_required)
     def get(self, request):
         return render(self.request, self.template_name, {})
-        
-
 
 
 class EmployeeProfileView(APIView):
     def get(self, request, format=None):
-        employee_profile = EmployeeProfile.objects.filter(user=self.request.user)
-        employee_profile = EmployeeProfileSerializer(employee_profile, many=True)
+        employee_profile = EmployeeProfile.objects.filter(
+            user=self.request.user)
+        employee_profile = EmployeeProfileSerializer(
+            employee_profile, many=True)
         return Response(employee_profile.data)
